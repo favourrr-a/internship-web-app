4d8ddd5d31b7faa67db9a242aac1d25d693ee4a4
nerdfavour <favourackonu13@gmail.com>
2024-05-24 21:36:51 +0000

    added mongoDB connection setup with mongoose

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://127.0.0.1:27017/users-info', {
+}).then(() => {
+    console.log("Successfully connected to MongoDB.");
+})
+    .catch((err) => {
+        console.error("Error connecting to MongoDB", err);
+        throw err;
+    });
+
